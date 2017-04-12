dae9be145cc70ddb7d00fd60900c3f3e150be377
Jonathan <jonathanprada@protonmail.ch>
2017-04-12 15:50:40 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(function(){
         currentCat: null,
         //Our array of cat objects
         cats: [
-        {"id": 1, "name": "Whiskers", "img": "img/cat1.jpg", "votes": 12},
+        {"id": 1, "name": "Whiskers", "img": "img/cat1.jpg", "votes": 0},
         {"id": 2, "name": "Chewie", "img": "img/cat2.jpg", "votes": 0},
         {"id": 3, "name": "Twins", "img": "img/cat3.jpg", "votes": 0},
         {"id": 4, "name": "Grumpy", "img": "img/cat4.jpg", "votes": 0},
@@ -19,6 +19,8 @@ $(function(){
             //Initialize our views
             ListView.init();
             CatView.init();
+            AdminView.init();
+            AdminView.hideAdmin();
         },
         //Returns our current cat
         getCurrentCat: function () {
@@ -36,6 +38,27 @@ $(function(){
         incrementCounter: function() {
         model.currentCat.votes++;
         CatView.render();
+        },
+        writeToModel: function(name, pic, votes) {
+            var id, i;
+
+            //Use the current cat id
+            id = model.currentCat.id;
+
+            //For each of the cats in storage
+            for (i = 0; i < model.cats.length; i++) {
+                // If our current cat id matches the id in the object
+                if (model.cats[i].id == id) {
+                    //Update name
+                    model.cats[i].name = name;
+                    //Update img
+                    model.cats[i].img = pic;
+                    //Update votes
+                    model.cats[i].votes = votes;
+                }
+            }
+            //Refresh the cat view
+            CatView.render();
         }
     };
 
@@ -51,6 +74,7 @@ $(function(){
             // on click, increment the current cat's counter
             this.CatPic.addEventListener('click', function(){
                 controller.incrementCounter();
+                AdminView.render();
             });
 
             //Render the view
@@ -99,6 +123,7 @@ $(function(){
                     return function() {
                         controller.setCurrentCat(catCopy);
                         CatView.render();
+                        AdminView.render();
                     };
                 })(cat));
 
@@ -108,6 +133,67 @@ $(function(){
         }
     };
 
+    var AdminView = {
+        init: function() {
+            //Our admin button for editing cat stuff
+            this.CatAdminButton = document.getElementById('admin');
+            //Our cancel button for stopping the editing of cat stuff
+            this.CatCancelButton = document.getElementById('cancel');
+            //Our save button for saving the editing cat of stuff
+            this.CatSaveButton = document.getElementById('save');
+            //Our cat name field
+            this.CatNameField = document.getElementById('cat-name-field');
+            //Our cat pic field
+            this.CatPicField = document.getElementById('cat-pic-field');
+            //Our cat votes field
+            this.CatVotesField = document.getElementById('cat-votes-field');
+
+            //Render the view
+            this.render()
+        },
+        render: function(){
+            //Variables we need
+            var interface, name, pic, votes;
+
+            //Get the current cat through the controller
+            var currentCat = controller.getCurrentCat();
+            this.CatNameField.value = currentCat.name;
+            this.CatPicField.value = currentCat.img;
+            this.CatVotesField.value = currentCat.votes;
+
+            //Event listener to toggle our admin view
+            this.CatAdminButton.addEventListener('click', function(){
+                if(interface){
+                    interface = false;
+                    document.getElementById("cat-admin-form").style.display="none";
+                } else {
+                    interface = true;
+                    document.getElementById("cat-admin-form").style.display="block";
+                }
+            });
+
+            //Event listener to toggle our admin view when cancelled
+            this.CatCancelButton.addEventListener('click', function(){
+                document.getElementById("cat-admin-form").style.display="none";
+            });
+
+            //Event listener to save changes to the cats
+            this.CatSaveButton.addEventListener('click', function(){
+                name = document.getElementById("cat-name-field").value;
+                pic = document.getElementById("cat-pic-field").value;
+                votes = document.getElementById("cat-votes-field").value;
+
+                //send to a function in controller, which stores to model
+                controller.writeToModel(name, pic, votes);
+
+            });
+
+        },
+        hideAdmin: function() {
+            this.interface = false;
+            document.getElementById("cat-admin-form").style.display="none";
+        }
+    };
 
     controller.init();
 });
